31e5fba27b24bf8ece3fed019260efff81d14a10
mkInGithub <228062032@qq.com>
2019-10-11 15:53:33 +0800

    aaa

--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 $(function(){
 	$("#submit").click(function(){
 		$.ajax({
@@ -12,31 +12,4 @@ $(function(){
 		})
 	})
 })
-=======
 
-
-$(function(){
-	$("#login").click(function(){
-		if($("#userid").val()!=null&&$("#password").val()!=null){
-		$.ajax({
-			type:"post",
-			url:"login",
-			data:$("#f1").serialize(),
-			datatype:"json",
-			sunccess:function(data){
-				if(User!=null){
-					alert("登录成功")
-				}else{
-					alert("登录失败")
-				}
-			}
-		})
-		}else{
-			alert("用户名或密码不能为空");
-		}
-		
-	})
-	
-	
-})
->>>>>>> branch 'master' of https://github.com/HaloFaction/RMS
