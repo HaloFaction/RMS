4a9550464bb7a2ae8bc53740b462fac785769ba4
ylh51666 <ylh2214471265@outlook.com>
2019-10-11 15:42:29 +0800

    vvv

--- src/main/java/com/res/RmsApplication.java
+++ src/main/java/com/res/RmsApplication.java
@@ -5,19 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-<<<<<<< HEAD
-=======
-
-
->>>>>>> branch 'master' of https://github.com/HaloFaction/RMS
 
 @SpringBootApplication
-<<<<<<< HEAD
-@ComponentScan("com.res")
 @MapperScan("com.res.model.mapper")
-=======
 @ComponentScan(basePackages="com.res")
->>>>>>> branch 'master' of https://github.com/HaloFaction/RMS
 @EnableTransactionManagement
 public class RmsApplication {
 
